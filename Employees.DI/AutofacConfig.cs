6a058439a64b1bf332c56c950ca977ba192dd24f
Mickael <MA>
2020-02-22 22:15:09 +0300

    Add DI Container

---
+++ Employees.DI/AutofacConfig.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using Employees.Repo;
+
+namespace Employees.DI
+{
+    public class DependencyResolver
+    {
+        private static DependencyResolver _instance;
+
+        public DependencyResolver Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new DependencyResolver());
+            }
+        }
+
+        private IContainer _container;
+        
+        private DependencyResolver()
+        {
+            ConfigureContainer();
+        }
+
+        private void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DepartmentRepository>().As<IDepartmentRepository>();
+            builder.RegisterType<EmployeeRepository>().As<IEmployeeRepository>();
+
+            _container = builder.Build();            
+        }
+
+        public T Resolve<T>() => _container.Resolve<T>();
+    }
+}
